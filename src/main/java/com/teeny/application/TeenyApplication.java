db6db111c27490b5b5bf6b8dd61be0db90707a4e
Safal Pandita <safalpandita@gmail.com>
2020-03-08 13:34:26 +0530

    CORS fix

--- src/main/java/com/teeny/application/TeenyApplication.java
+++ src/main/java/com/teeny/application/TeenyApplication.java
@@ -9,6 +9,11 @@ import io.dropwizard.db.PooledDataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class TeenyApplication extends Application<ApplicationConfiguration> {
 	
@@ -40,6 +45,17 @@ public class TeenyApplication extends Application<ApplicationConfiguration> {
 		final ApplicationResource resource = new ApplicationResource(
 				teenyDao
 		);
+		// Enable CORS headers
+		final FilterRegistration.Dynamic cors =
+				environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+		// Configure CORS parameters
+		cors.setInitParameter("allowedOrigins", "*");
+		cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+		cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+		// Add URL mapping
+		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 		
 		environment.jersey().register(resource);
 	}
