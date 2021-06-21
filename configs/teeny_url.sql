b6654eef6969616c572345a1225e62a7469238c0
Safal Pandita <safalpandita@gmail.com>
2021-06-22 03:56:44 +0530

    Modify sql for backups

---
+++ configs/teeny_url.sql
@@ -0,0 +1,44 @@
+-- -------------------------------------------------------------
+-- TablePlus 3.1.2(296)
+--
+-- https://tableplus.com/
+--
+-- Database: teeny
+-- Generation Time: 2021-06-22 03:50:14.6480
+-- -------------------------------------------------------------
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+DROP TABLE IF EXISTS `teeny_url`;
+CREATE TABLE `teeny_url` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `teenyKey` varchar(2083) DEFAULT NULL,
+  `url` varchar(2083) NOT NULL,
+  `created_on` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `teeny_url` (`id`, `teenyKey`, `url`, `created_on`) VALUES
+('1', 'b', 'http://www.cricbuzz.com', '1624313580366'),
+('2', 'c', 'http://www.google.com', '1624313609163'),
+('3', 'piss', 'http://www.pulkitisapussy.com', '1624313632268'),
+('4', 'rand', 'http://www.random.com', '1624313690903');
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
